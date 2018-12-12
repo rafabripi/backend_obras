abe976f1b454308577f5708b591aa84f4e20a544
rafabripi <rafabripi@users.noreply.github.com>
2018-12-11 20:37:39 -0600

    modificacion de asignacion de puerto para heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('./server/config');
 var mongoose = require('mongoose');
 var app = require('./app');
+var port = process.env.PORT;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/Control_Obras')
@@ -9,8 +10,8 @@ mongoose.connect('mongodb://localhost:27017/Control_Obras')
         	console.log("Conexión a la base de datos Control_Obras...");
 
         	// Creacion del servidor
-        	app.listen(process.env.PORT, () => {
-        		console.log(`Server engine at url: localhost:${process.env.PORT}`);
+        	app.listen(port, () => {
+        		console.log(`Server engine at url: localhost:${port}`);
         	});
         })
         .catch(err => console.log(err));
