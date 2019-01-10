6a1087e75399ee4951322a8740be08858d28702a
Rafael Bribiesca P <rafabripi@gmail.com>
2019-01-10 15:55:56 -0600

    creacion de metodo getAvance

--- controllers/avance_obras.js
+++ controllers/avance_obras.js
@@ -27,6 +27,24 @@ var controller = {
                 avance: avanceStored
             });
         });
+    },
+
+    getAvance: function (req, res) {
+        let avanceId = req.params.id;
+
+        Avance.findById(avanceId, (err, avanceDB) => {
+            if (err) {
+                return res.status(500).json({message: 'Error interno'});
+            }
+            if (!avanceDB) {
+                return res.status(404).json({message: 'No se encontro'});
+            }
+            return res.status(200).json({
+                ok: true,
+                avanceDB,
+                usuarioToken: req.usuario
+            });
+        });
     }
 }
 
--- routes/avance_obras.js
+++ routes/avance_obras.js
@@ -5,5 +5,6 @@ var AvanceController = require ('../controllers/avance_obras');
 const {verificarToken} = require('../server/middelwares/autenticacion');
 
 router.post('/saveAvance', verificarToken, AvanceController.saveAvance);
+router.get('/getAvance/:id', verificarToken, AvanceController.getAvance);
 
 module.exports = router;
